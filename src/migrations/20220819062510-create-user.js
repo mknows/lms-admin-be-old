0035fb0fa917e5c07c08ac39de16d32cfe05c6e7
bennu7 <laluibnuhidayatullah@gmail.com>
2022-08-19 13:29:02 +0700

    add model user

---
+++ src/migrations/20220819062510-create-user.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      level: {
+        type: Sequelize.STRING
+      },
+      gambar: {
+        type: Sequelize.STRING
+      },
+      no_hp: {
+        type: Sequelize.STRING
+      },
+      firebaseUID: {
+        type: Sequelize.STRING
+      },
+      dosen_akademik: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
