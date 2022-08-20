e5808b2549e6edf0a0af0c55229f438bc123c31c
bennu7 <laluibnuhidayatullah@gmail.com>
2022-08-20 09:48:05 +0700

    add firebase auth and db pg to login & register user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,11 @@
 const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const admin = require("firebase-admin");
+const serviceAccount = require("../../serviceAccountKey_test-express-auth-firebase-adminsdk-vnzdj-17db49ab3a.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = {
   index: async (req, res) => {
@@ -6,6 +13,61 @@ module.exports = {
       const data = await User.findAll();
 
       res.sendJson(200, true, "sucess get all data", data);
+    } catch (err) {
+      res.sendJson(500, false, err.message, null);
+    }
+  },
+
+  create: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+
+      const createAuthFirebase = await admin.auth().createUser({
+        email,
+        password,
+        emailVerified: false,
+        disabled: false,
+      });
+
+      const hashPassword = bcrypt.hashSync(password, 10);
+
+      const created = await User.create({
+        name,
+        email,
+        password: hashPassword,
+        firebaseUID: createAuthFirebase.uid,
+      });
+
+      res.sendJson(201, true, "success create new user", {
+        id: created.id,
+        name: created.name,
+        email: created.email,
+      });
+    } catch (err) {
+      res.sendJson(500, false, err.message, null);
+    }
+  },
+
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        return res.sendJson(404, false, "email tidak ditemukan", null);
+      }
+
+      const passwordCorrect = await bcrypt.compare(password, user.password);
+      if (!passwordCorrect) {
+        return res.sendJson(400, false, "password salah", null);
+      }
+
+      res.sendJson(200, true, "login success", user);
     } catch (err) {
       res.sendJson(500, false, err.message, null);
     }
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,5 +3,7 @@ const route = express.Router();
 const userController = require("../controllers/userController");
 
 route.get("/", userController.index);
+route.get("/login", userController.login);
+route.post("/", userController.create);
 
 module.exports = route;
