4a0a564494d1db6c8646b836d18c1d5f1695e6d5
ary-bod <ibhodro123@gmail.com>
2022-08-24 11:33:21 +0700

    add profile route, controller

---
+++ src/routes/Profile.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const route = express.Router();
+
+const profileController = require("../controllers/profileController");
+const { validate, validatorMessage } = require('../middlewares/Validator');
+const { protection } = require("../middlewares/Authentication");
+
+route.get("/me", protection, profileController.getMe);
+route.put('/me', protection, validate('updateDataUser'), validatorMessage, profileController.updateMe);
+
+module.exports = route;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const route = express.Router();
 
 const userRoute = require("./Auth");
+const profileRoute = require("./Profile");
 
+route.use("/profile", profileRoute);
 route.use("/auth", userRoute);
 
 module.exports = route;
