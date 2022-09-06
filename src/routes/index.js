de2efc4dec126dd39db9af5270dab5e5da1b8011
qwertyuioplukas@gmail.com <qwertyuioplukas@gmail.com>
2022-09-06 11:06:03 +0700

    added create

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,15 @@ const route = express.Router();
 const userRoute = require("./auth");
 const profileRoute = require("./Profile");
 const adminRoute = require("./admin");
-const forumRoute = require("./forum")
-const studikuRoute = require("./studiku")
+const forumRoute = require("./forum");
+const studikuRoute = require("./studiku");
+const createRoute = require("./create");
 
 route.use("/studiku", studikuRoute);
 route.use("/profile", profileRoute);
 route.use("/auth", userRoute);
 route.use("/admin", adminRoute);
-route.use("/forum",forumRoute)
+route.use("/forum",forumRoute);
+route.use("/create", createRoute);
 
 module.exports = route;
