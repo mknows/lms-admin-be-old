f4e9e14ad554b0c2fa729822d6b4fff358ecf7cf
qwertyuioplukas@gmail.com <qwertyuioplukas@gmail.com>
2022-09-08 13:42:03 +0700

    studiku routes repositioning

--- src/routes/studiku.js
+++ src/routes/studiku.js
@@ -5,14 +5,16 @@ const studikuController = require("../controllers/studikuController");
 const { protection } = require("../middlewares/Authentication");
 
 route.get("/allsubject", studikuController.getAllSubject);
-route.get("/getModule/:id", protection, studikuController.getModule)
-route.get("/getQuizDesc/:id", protection, studikuController.getQuizDesc)
-route.get("/getSubjectForStudent", protection, studikuController.getSubjectForStudent)
-route.get("/session/:sub_id", protection, studikuController.getAllSessionInSubject)
+route.get("/getSubjectForStudent", protection, studikuController.getSubjectForStudent);
+route.post("/takeSubject", protection, studikuController.takeSubject);
 
-route.post("/makeQuiz", protection, studikuController.makeQuiz)
-route.post("/takeQuiz/:id", protection, studikuController.takeQuiz)
-route.post("/postQuizAnswer", protection, studikuController.postQuizAnswer)
-route.post("/takeSubject", protection, studikuController.takeSubject)
+route.get("/getModule/:id", protection, studikuController.getModule);
+
+route.get("/session/:sub_id", protection, studikuController.getAllSessionInSubject);
+
+route.get("/getQuizDesc/:id", protection, studikuController.getQuizDesc);
+route.post("/makeQuiz", protection, studikuController.makeQuiz);
+route.post("/takeQuiz/:id", protection, studikuController.takeQuiz);
+route.post("/postQuizAnswer", protection, studikuController.postQuizAnswer);
 
 module.exports = route;
