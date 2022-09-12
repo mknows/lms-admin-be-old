db9cdbe7d3273ec957156eaa9d8702876f78b569
qwertyuioplukas@gmail.com <qwertyuioplukas@gmail.com>
2022-09-12 17:01:54 +0700

    ua had differing capitalization

--- src/migrations/20220901044542-create-user-activity.js
+++ src/migrations/20220901044542-create-user-activity.js
@@ -47,6 +47,6 @@ module.exports = {
 		});
 	},
 	async down(queryInterface, Sequelize) {
-		await queryInterface.dropTable("User_Activities");
+		await queryInterface.dropTable("user_activities");
 	},
 };
