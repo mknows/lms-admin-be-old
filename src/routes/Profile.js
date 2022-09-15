f310e351010f0fdc7719cd82213dc0f5ed358b2b
bennu7 <laluibnuhidayatullah@gmail.com>
2022-09-15 13:07:03 +0700

    fix update using image multer & gitignore

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -52,6 +52,7 @@ route.get("/me", protection, authorize("user"), profileController.getMe);
 route.put(
 	"/me",
 	protection,
+    upload.single('image'),
 	authorize("user"),
 	validate("updateDataUser"),
 	validatorMessage,
