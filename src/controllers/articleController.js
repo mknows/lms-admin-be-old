87dc40125aff39921ea6131de2be70a18ca858ed
bennu7 <laluibnuhidayatullah@gmail.com>
2022-09-22 10:09:29 +0700

    continuing firebase article

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -1,150 +1,169 @@
 const { Article } = require("../models");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const admin = require("firebase-admin");
+const {
+  getStorage,
+  ref,
+  getDownloadURL,
+  deleteObject,
+} = require("firebase/storage");
 
 module.exports = {
-	/**
-	 * @desc      Get All data Article
-	 * @route     GET /api/v1/article/index
-	 * @access    Private
-	 */
-	index: async (req, res) => {
-		try {
-			const data = await Article.findAll();
-			if (data.length == 0) {
-				return res.sendJson(
-					200,
-					true,
-					"success get, but not have data article"
-				);
-			}
+  /**
+   * @desc      Get All data Article
+   * @route     GET /api/v1/article/index
+   * @access    Private
+   */
+  index: async (req, res) => {
+    try {
+      const data = await Article.findAll();
+      if (data.length == 0) {
+        return res.sendJson(
+          200,
+          true,
+          "success get, but not have data article"
+        );
+      }
 
-			return res.sendJson(200, true, "success get all data", data);
-		} catch (error) {
-			console.log(error);
-			return res.sendJson(403, false, error, {});
-		}
-	},
+      return res.sendJson(200, true, "success get all data", data);
+    } catch (error) {
+      console.log(error);
+      return res.sendJson(403, false, error, {});
+    }
+  },
 
-	/**
-	 * @desc      Create a new data article
-	 * @route     POST /api/v1/article/create
-	 * @access    Private
-	 */
-	create: async (req, res) => {
-		try {
-			const { title, description } = req.body;
+  /**
+   * @desc      Create a new data article
+   * @route     POST /api/v1/article/create
+   * @access    Private
+   */
+  create: async (req, res) => {
+    try {
+      const { title, description } = req.body;
+      console.log(req.file);
+      const bucket = admin.storage().bucket();
 
-			const created = await Article.create({
-				title,
-				description,
-				image: req.file.filename,
-			});
+      const articleFile =
+        uuidv4() + "-" + req.file.originalname.split(" ").join("-");
+      const articleBuffer = req.file.buffer;
 
-			return res.sendJson(201, true, "success create new article", {
-				title: created.title,
-				description: created.description,
-				image: created.image,
-			});
-		} catch (error) {
-			console.log(error);
-			return res.sendJson(403, false, error, {});
-		}
-	},
+      bucket
+        .file(`images/article/${articleFile}`)
+        .createWriteStream()
+        .end(articleBuffer);
 
-	/**
-	 * @desc      Update data article
-	 * @route     PUT /api/v1/article/update
-	 * @access    Private
-	 */
-	update: async (req, res) => {
-		try {
-			const { title, description } = req.body;
-			const { uuid } = req.params;
+      const created = await Article.create({
+        title,
+        description,
+        image: `images/article/${articleFile}`,
+      });
 
-			const findFile = await Article.findOne({
-				where: {
-					id: uuid,
-				},
-			});
+      return res.sendJson(201, true, "success create new article", {
+        title: created.title,
+        description: created.description,
+        image: created.image,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.sendJson(403, false, error, {});
+    }
+  },
 
-			if (req.file) {
-				if (findFile == null) {
-					fs.unlinkSync("./public/images/" + req.file.filename);
-					return res.sendJson(400, false, "can't find article");
-				}
+  /**
+   * @desc      Update data article
+   * @route     PUT /api/v1/article/update
+   * @access    Private
+   */
+  update: async (req, res) => {
+    try {
+      const { title, description } = req.body;
+      const { uuid } = req.params;
 
-				fs.unlinkSync("./public/images/" + findFile.image);
-				await Article.update(
-					{
-						image: req.file.filename,
-					},
-					{
-						where: {
-							id: uuid,
-						},
-					}
-				);
-			}
+      const findFile = await Article.findOne({
+        where: {
+          id: uuid,
+        },
+      });
 
-			if (findFile == null) {
-				return res.sendJson(400, false, "can't find article");
-			}
-			const updated = await Article.update(
-				{
-					title,
-					description,
-				},
-				{
-					where: {
-						id: uuid,
-					},
-				}
-			);
+      if (req.file) {
+        if (findFile == null) {
+          fs.unlinkSync("./public/images/" + req.file.filename);
+          return res.sendJson(400, false, "can't find article");
+        }
 
-			if (updated == 0) {
-				return res.sendJson(
-					204,
-					false,
-					"can't update because there no have value article"
-				);
-			}
-			return res.sendJson(200, true, "succes update article");
-		} catch (error) {
-			console.log(error);
-			return res.sendJson(403, false, error, {});
-		}
-	},
+        fs.unlinkSync("./public/images/" + findFile.image);
+        await Article.update(
+          {
+            image: req.file.filename,
+          },
+          {
+            where: {
+              id: uuid,
+            },
+          }
+        );
+      }
 
-	/**
-	 * @desc      Delete data article
-	 * @route     DELETE /api/v1/article/delete/:id
-	 * @access    Private
-	 */
-	delete: async (req, res) => {
-		try {
-			const { id } = req.params;
+      if (findFile == null) {
+        return res.sendJson(400, false, "can't find article");
+      }
+      const updated = await Article.update(
+        {
+          title,
+          description,
+        },
+        {
+          where: {
+            id: uuid,
+          },
+        }
+      );
 
-			const findArticle = await Article.findOne({
-				where: {
-					id,
-				},
-			});
+      if (updated == 0) {
+        return res.sendJson(
+          204,
+          false,
+          "can't update because there no have value article"
+        );
+      }
+      return res.sendJson(200, true, "succes update article");
+    } catch (error) {
+      console.log(error);
+      return res.sendJson(403, false, error, {});
+    }
+  },
 
-			if (findArticle == null) {
-				return res.sendJson(400, false, "article not found");
-			}
+  /**
+   * @desc      Delete data article
+   * @route     DELETE /api/v1/article/delete/:id
+   * @access    Private
+   */
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-			fs.unlinkSync("./public/images/" + findArticle.image);
-			await Article.destroy({
-				where: {
-					id,
-				},
-			});
+      const findArticle = await Article.findOne({
+        where: {
+          id,
+        },
+      });
 
-			return res.sendJson(200, true, "success delete article");
-		} catch (error) {
-			console.log(error);
-			return res.sendJson(403, false, error, {});
-		}
-	},
+      if (findArticle == null) {
+        return res.sendJson(400, false, "article not found");
+      }
+
+      fs.unlinkSync("./public/images/" + findArticle.image);
+      await Article.destroy({
+        where: {
+          id,
+        },
+      });
+
+      return res.sendJson(200, true, "success delete article");
+    } catch (error) {
+      console.log(error);
+      return res.sendJson(403, false, error, {});
+    }
+  },
 };
