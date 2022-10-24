321743c4d3833a3387d932ec75a57316bc0355a0
qwertyuioplukas@gmail.com <qwertyuioplukas@gmail.com>
2022-10-24 10:26:08 +0700

    add authorize student

--- src/routes/module.js
+++ src/routes/module.js
@@ -4,35 +4,88 @@ const moduleController = require("../controllers/moduleController");
 const { protection, authorize } = require("../middlewares/Authentication");
 
 // Module
-route.get("/", protection, moduleController.getAllModule);
-route.post("/create", protection, moduleController.createModule);
-route.put("/edit/:module_id", protection, moduleController.editModule);
-route.delete("/delete/:module_id", protection, moduleController.deleteModule);
-route.get("/:id", protection, moduleController.getModule);
+route.get("/", protection, authorize("student"), moduleController.getAllModule);
+route.post(
+	"/create",
+	protection,
+	authorize("student"),
+	moduleController.createModule
+);
+route.put(
+	"/edit/:module_id",
+	protection,
+	authorize("student"),
+	moduleController.editModule
+);
+route.delete(
+	"/delete/:module_id",
+	protection,
+	authorize("student"),
+	moduleController.deleteModule
+);
+route.get("/:id", protection, authorize("student"), moduleController.getModule);
 
 // Video
-route.post("/createvideo", protection, moduleController.createVideo);
-route.get("/video", protection, moduleController.getAllVideo);
-route.put("/video/edit/:video_id", protection, moduleController.editVideo);
-route.get("/video/:id", protection, moduleController.getVideo);
+route.post(
+	"/createvideo",
+	protection,
+	authorize("student"),
+	moduleController.createVideo
+);
+route.get(
+	"/video",
+	protection,
+	authorize("student"),
+	moduleController.getAllVideo
+);
+route.put(
+	"/video/edit/:video_id",
+	protection,
+	authorize("student"),
+	moduleController.editVideo
+);
+route.get(
+	"/video/:id",
+	protection,
+	authorize("student"),
+	moduleController.getVideo
+);
 route.delete(
 	"/video/delete/:video_id",
 	protection,
+	authorize("student"),
 	moduleController.deleteVideo
 );
 
 // Document
-route.post("/createdocument", protection, moduleController.createDocument);
-route.get("/document", protection, moduleController.getAllDocument);
+route.post(
+	"/createdocument",
+	protection,
+	authorize("student"),
+	moduleController.createDocument
+);
+route.get(
+	"/document",
+	protection,
+	authorize("student"),
+	moduleController.getAllDocument
+);
 route.put(
 	"/document/edit/:document_id",
 	protection,
+	authorize("student"),
 	moduleController.editDocument
 );
-route.get("/document/:id", protection, moduleController.getDocument);
+route.get(
+	"/document/:id",
+	protection,
+	authorize("student"),
+	moduleController.getDocument
+);
 route.delete(
 	"/document/delete/:document_id",
 	protection,
+	authorize("student"),
 	moduleController.deleteDocument
 );
 
