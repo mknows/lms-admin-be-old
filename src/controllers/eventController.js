f7dbf26b5ffd166b05487414cd2898f4e24a4d8e
Kenniskiu Fortino Kurniawan <kenniskiufk123@gmail.com>
2022-12-13 12:35:06 +0700

    achievements controller

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,4 +1,12 @@
-const { Event, StudentEvent } = require("../models");
+const {
+	Event,
+	StudentEvent,
+	Student,
+	Session,
+	DiscussionForum,
+	Reply,
+	Comment,
+} = require("../models");
 const asyncHandler = require("express-async-handler");
 const { Op } = require("sequelize");
 const pagination = require("../helpers/pagination");
@@ -31,12 +39,14 @@ module.exports = {
 	joinEvent: asyncHandler(async (req, res) => {
 		const student_id = req.student_id;
 		const { event_id } = req.params;
+
 		const student_event = await StudentEvent.findOne({
 			where: {
 				student_id,
 				event_id,
 			},
 		});
+
 		if (student_event) {
 			return res.sendJson(
 				400,
