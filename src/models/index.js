6ee5a18564fab5f064a779525c9d459f88e5275e
qwertyuioplukas@gmail.com <qwertyuioplukas@gmail.com>
2023-01-18 11:49:41 +0700

    config

--- src/models/index.js
+++ src/models/index.js
@@ -6,54 +6,53 @@ require("dotenv").config();
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
+const config = require("../config/config.js")[env];
 
 const databases = {};
 const files = [];
 
 let sequelize;
 
-if (config.use_env_variable)
+if (config.use_env_variable) {
 	sequelize = new Sequelize(process.env[config.use_env_variable], config);
-else
+} else {
 	sequelize = new Sequelize(
 		config.database,
 		config.username,
 		config.password,
 		config
 	);
+}
 
 const sortDir = (mainDirectory) => {
-  const folders = [];
-  const CheckFile = filePath => (fs.statSync(filePath).isFile());
-  const sortPath = (directory) => {
-    fs
-      .readdirSync(directory)
-      .filter(file => (file.indexOf(".") !== 0) && (file !== "index.js"))
-      .forEach((res) => {
-        const filePath = path.join(directory, res);
-        if (CheckFile(filePath)) files.push(filePath);
-        else folders.push(filePath);
-      });
-  };
+	const folders = [];
+	const CheckFile = (filePath) => fs.statSync(filePath).isFile();
+	const sortPath = (directory) => {
+		fs.readdirSync(directory)
+			.filter((file) => file.indexOf(".") !== 0 && file !== "index.js")
+			.forEach((res) => {
+				const filePath = path.join(directory, res);
+				if (CheckFile(filePath)) files.push(filePath);
+				else folders.push(filePath);
+			});
+	};
 
-  folders.push(mainDirectory);
+	folders.push(mainDirectory);
 
-  let i = 0;
+	let i = 0;
 
-  do {
-    sortPath(folders[i]);
-    i += 1;
-  } while (i < folders.length);
+	do {
+		sortPath(folders[i]);
+		i += 1;
+	} while (i < folders.length);
 };
 
 sortDir(__dirname);
 
-files
-  .forEach((file) => {
-    const model = require(file)(sequelize, Sequelize.DataTypes);
-    databases[model.name] = model;
-  });
+files.forEach((file) => {
+	const model = require(file)(sequelize, Sequelize.DataTypes);
+	databases[model.name] = model;
+});
 
 Object.keys(databases).forEach((tableName) => {
 	if (databases[tableName].associate) {
